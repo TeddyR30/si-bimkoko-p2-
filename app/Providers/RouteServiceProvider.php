19a4822af7a6240c579580b4db73d30c8fbb13c4
TeddyR30 <Synyster1213@gmail.com>
2023-11-11 16:36:26 +0700

    keenam

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/logins';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
